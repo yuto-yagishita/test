da1cab5877b54c3e9b32952aaa51fa43bf8b2d19
yuto-yagishita <x20u043@mitsuhashi.ac.jp>
2023-10-18 11:28:50 +0900

    Merge branch 'master' of https://github.com/yuto-yagishita/1018_Body
    # Conflicts:
    #	app/src/main/java/com/example/body/DBtest.java
    #変更しました

--- app/src/main/java/com/example/body/DBtest.java
+++ app/src/main/java/com/example/body/DBtest.java
@@ -2,60 +2,61 @@ package com.example.body;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.database.Cursor;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
-
 public class DBtest extends AppCompatActivity {
+    private CalendarView calendarView;
+    private long selectedDateMillis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dbtest);
 
-        DatabaseManager dbManager = new DatabaseManager(this);
-        dbManager.open();
+        calendarView = findViewById(R.id.calendarView);
 
-// データの挿入
-//        dbManager.insertData("オートミール",150.0);
-//        dbManager.insertData("ヨーグルト", 100.0);
-
- //データの取得
-        Cursor cursor = dbManager.fetchData();
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                do {
-                    String food_name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.FOOD_NAME));
-                    Double food_krl = cursor.getDouble(cursor.getColumnIndex(DatabaseHelper.FOOD_KRL));
-                    // データを処理
-                    System.out.println(food_name);
-                    System.out.println(food_krl);
-                } while (cursor.moveToNext());
+        // ボタン
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // ボタンがクリックされたときの処理
+                toastMake(getFormattedDate(selectedDateMillis), 0, 500);
             }
-            cursor.close();
-        }
-        dbManager.close();
+        });
 
-        //CalenderView
-        ((CalendarView) findViewById(R.id.calendarView)).setOnDateChangeListener(listener);
+        // CalendarViewの日付変更リスナーを設定
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // 日付が選択されたときの処理
+                // 選択された日付のミリ秒単位の値を計算
+                selectedDateMillis = getDateInMillis(year, month, dayOfMonth);
+            }
+        });
     }
 
-    CalendarView.OnDateChangeListener listener = new CalendarView.OnDateChangeListener() {
-        @Override
-        public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-            // 日付が選択されたときの処理
-            String selectedDate = year + "年" + (month + 1) + "月" + dayOfMonth + "日"; // 月は0から11の範囲なので+1する
-            toastMake(selectedDate,0,500);
-        }
-    };
+    // 年月日からミリ秒単位の日付を取得
+    private long getDateInMillis(int year, int month, int dayOfMonth) {
+        java.util.Calendar calendar = java.util.Calendar.getInstance();
+        calendar.set(year, month, dayOfMonth);
+        return calendar.getTimeInMillis();
+    }
 
-    //トースト表示
-    private void toastMake(String message , int x, int y){
-        Toast toast = Toast.makeText(this,message,Toast.LENGTH_LONG);
-        //位置調整
-        toast.setGravity(Gravity.CENTER, x ,y);
+    // ミリ秒単位の日付をフォーマットして文字列に変換
+    private String getFormattedDate(long dateMillis) {
+        String selectedDate = "選択された日付: " + android.text.format.DateFormat.format("yyyy年MM月dd日", dateMillis);
+        return selectedDate;
+    }
+
+    // トースト表示する
+    private void toastMake(String message, int x, int y) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        // 位置調整
+        toast.setGravity(Gravity.CENTER, x, y);
         toast.show();
     }
 }
